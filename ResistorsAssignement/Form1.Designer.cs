25e5731869dd8acd9b2740fb46adbea7ec53e15b
Caine Atkinson <rc0033222@student.riversidecollege.ac.uk>
2018-10-16 09:41:17 +0100

    Homescreen complete

--- ResistorsAssignement/Form1.Designer.cs
+++ ResistorsAssignement/Form1.Designer.cs
@@ -30,19 +30,60 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1s = new System.Windows.Forms.Button();
+            this.button2p = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Black;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Franklin Gothic Book", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(60, 188);
+            this.label1.Location = new System.Drawing.Point(12, 46);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(337, 25);
+            this.label1.Size = new System.Drawing.Size(439, 34);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Welcome to the resistor calcutalor";
+            this.label1.Text = "Welcome to the resistor calculator";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ResistorsAssignement.Properties.Resources.resistor_diagram;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 229);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(434, 187);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1s
+            // 
+            this.button1s.BackColor = System.Drawing.Color.White;
+            this.button1s.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button1s.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1s.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1s.ForeColor = System.Drawing.Color.Black;
+            this.button1s.Location = new System.Drawing.Point(55, 127);
+            this.button1s.Name = "button1s";
+            this.button1s.Size = new System.Drawing.Size(105, 65);
+            this.button1s.TabIndex = 2;
+            this.button1s.Text = "Series";
+            this.button1s.UseVisualStyleBackColor = false;
+            // 
+            // button2p
+            // 
+            this.button2p.BackColor = System.Drawing.Color.White;
+            this.button2p.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2p.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2p.ForeColor = System.Drawing.Color.Black;
+            this.button2p.Location = new System.Drawing.Point(298, 127);
+            this.button2p.Name = "button2p";
+            this.button2p.Size = new System.Drawing.Size(100, 65);
+            this.button2p.TabIndex = 3;
+            this.button2p.Text = "Parallel";
+            this.button2p.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
@@ -50,12 +91,16 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(458, 450);
+            this.Controls.Add(this.button2p);
+            this.Controls.Add(this.button1s);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
             this.Text = "Resistor Calculator";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -64,6 +109,9 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1s;
+        private System.Windows.Forms.Button button2p;
     }
 }
 
